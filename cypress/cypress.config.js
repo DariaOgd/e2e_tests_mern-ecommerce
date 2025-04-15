4075e39010a22bca53100ecbae9e4c0bac3c9e9d
DariaOgd <daria.ogd@gmail.com>
2025-04-15 21:08:43 +0200

    config file update

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -2,8 +2,25 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:8000',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
+    specPattern: 'cypress/e2e/**/*.cy.js', 
+    supportFile: 'cypress/support/e2e.js',
+  },
+
+  viewportWidth: 1280,
+  viewportHeight: 720,
+
+  defaultCommandTimeout: 8000,
+  requestTimeout: 10000,
+  responseTimeout: 10000,
+
+  video: false,
+  screenshotOnRunFailure: true,
+
+  retries: {
+    runMode: 1,
+    openMode: 0,
   },
 });
